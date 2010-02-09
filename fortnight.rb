f6d493b0c0f25dc074c212ef21a2496b53c5ebb9
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-02-09 11:47:28 -0800

    initial

---
+++ fortnight.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'haml'
+
+get '/' do
+  haml :index
+end
+
+get '/portfolio' do
+  haml :portfolio
+end
+
+get '/contact' do
+  haml :contact
+end
+
+get '/about' do
+  haml :about
+end
+
+get '/application.css' do
+  content_type 'text/css'
+  sass :stylesheet
+end
