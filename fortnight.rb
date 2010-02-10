808ae559e267c317bca0b38d6c4f0c6fbbe0d445
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-02-09 21:10:06 -0800

    text changes

--- fortnight.rb
+++ fortnight.rb
@@ -2,20 +2,39 @@ require 'sinatra'
 require 'haml'
 require 'sass'
 
+# turn on smartypants by default
+class RDiscount
+  def initialize text, *extensions
+    @text = text
+    @smart = true
+    @filter_styles = nil
+    @filter_html = nil
+    @fold_lines = nil
+    extensions.each { |e| e.send("#{e}=", true) }
+  end
+end
+Markdown = RDiscount
+
+set :sass, :style => :compressed
+
+def h template
+  haml @template = template
+end
+
 get '/' do
-  haml :index
+  h :index
 end
 
 get '/portfolio' do
-  haml :portfolio
+  h :portfolio
 end
 
 get '/contact' do
-  haml :contact
+  h :contact
 end
 
 get '/about' do
-  haml :about
+  h :about
 end
 
 get '/application.css' do
