78684bc1a2df56d1b1b49d975b11dee4aab860e4
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-02-10 17:30:49 -0800

    changes to get working on heroku

--- config.ru
+++ config.ru
@@ -1,4 +1,9 @@
-require 'fortnight'
+begin
+  require File.dirname(__FILE__) + '/vendor/gems/environment'
+  Bundler.require_env
+rescue LoadError
+end
 
+require 'fortnight'
 Sinatra::Application.set :raise_errors, true
 run Sinatra::Application
--- fortnight.rb
+++ fortnight.rb
@@ -2,19 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'sass'
-
-# turn on smartypants by default
-class RDiscount
-  def initialize text, *extensions
-    @text = text
-    @smart = true
-    @filter_styles = nil
-    @filter_html = nil
-    @fold_lines = nil
-    extensions.each { |e| e.send("#{e}=", true) }
-  end
-end
-Markdown = RDiscount
+require 'rdiscount'
 
 set :sass, :style => :compressed
 
